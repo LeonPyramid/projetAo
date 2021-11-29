7b6f42eba36d07cae0789cb865a0a82a49203757
laetysful <laetys33@gmail.com>
2021-11-29 12:14:21 +0100

    Javadoc + Enum theaterstatus

---
+++ culturalprogramation/src/local/culturalprogramation/domain/Theater.java
@@ -0,0 +1,11 @@
+package local.culturalprogramation.domain;
+
+public class Theater {
+    private int theaterCapacity;
+    //hash map date -> Event
+    //Horaire d'ouverture
+
+    public int getTheaterCapacity() {
+        return theaterCapacity;
+    }
+}
---
+++ culturalprogramation/src/local/culturalprogramation/domain/TheaterStatus.java
@@ -0,0 +1,25 @@
+
+package local.culturalprogramation.domain;
+
+/**
+ *  Enumerator used to define the status of a theater
+ * 
+ */
+
+public enum TheaterStatus {
+    /**
+     *  The theather is open and empty
+     */
+    OPEN,
+    
+    /**
+     *  The theather is closed
+     */
+    CLOSED,
+    
+    /**
+     *  The theather is occupied
+     */
+    OCCUPIED;
+    
+}
