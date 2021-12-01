665a4e3e0c1fbcdb43a7271bbe6a520b975b8751
rdelpy <romain.delpy@eut.u-bordeaux.fr>
2021-12-01 18:06:33 +0100

    LoadFileOfOpeningHours

--- culturalprogramation/src/local/culturalprogramation/domain/theater/WeeklyOpeningHours.java
+++ culturalprogramation/src/local/culturalprogramation/domain/theater/WeeklyOpeningHours.java
@@ -1,6 +1,11 @@
 package local.culturalprogramation.domain.theater;
 
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Hashtable;
 import java.util.List;
 
@@ -60,5 +65,124 @@ public class WeeklyOpeningHours {
             return ret;
         }
         return null;
+    }
+
+    public void loadPlanningFile(String path){
+        try{
+            Path lPath = Paths.get(path);
+            BufferedReader reader = Files.newBufferedReader(lPath);
+            String line = null;
+            Boolean isEdited[] = new Boolean[7];
+            for(int i = 0; i < 7 ; i ++){
+                isEdited[i] = false;
+            }
+            WeeklyOpeningHours localWoh = new WeeklyOpeningHours();
+            while((line = reader.readLine())!= null){
+                if(!line.matches("[A-Z]{3}\\s\\[\\d{2}\\:\\d{2}\\,\\d{2}\\:\\d{2}\\]")){
+                    System.err.println("File formart not matching a planning format: DAY [HH:MM,hh:mm]");
+                    return;
+                }
+                line.replace("[", "");
+                line.replace("]", "");
+                String split[] = line.split("\\s|\\:|\\,");
+                int day = -1;
+                switch (split[0]){
+                    case "MON":
+                        if(isEdited[0]){
+                            System.err.println("MON has already been loaded from this file!");
+                            return;
+                        }
+                        isEdited[0] = true;
+                        day = Calendar.MONDAY;
+                    break;
+                    case "TUE":
+                        if(isEdited[1]){
+                            System.err.println("MON has already been loaded from this file!");
+                            return;
+                        }
+                        isEdited[1] = true;
+                        day = Calendar.TUESDAY;
+                    break;
+                    case "WED":
+                        if(isEdited[2]){
+                            System.err.println("MON has already been loaded from this file!");
+                            return;
+                        }
+                        isEdited[2] = true;
+                        day = Calendar.WEDNESDAY;
+                    break;
+                    case "THU":
+                        if(isEdited[3]){
+                            System.err.println("MON has already been loaded from this file!");
+                            return;
+                        }
+                        isEdited[3] = true;
+                        day = Calendar.THURSDAY;
+                    break;
+                    case "FRI":
+                        if(isEdited[4]){
+                            System.err.println("MON has already been loaded from this file!");
+                            return;
+                        }
+                        isEdited[4] = true;
+                        day = Calendar.FRIDAY;
+                    break;
+                    case "SAT":
+                        if(isEdited[5]){
+                            System.err.println("MON has already been loaded from this file!");
+                            return;
+                        }
+                        isEdited[5] = true;
+                        day = Calendar.SATURDAY;
+                    break;
+                        case "SUN":
+                        if(isEdited[6]){
+                            System.err.println("MON has already been loaded from this file!");
+                            return;
+                        }
+                        isEdited[6] = true;
+                        day = Calendar.SUNDAY;
+                    break;
+
+                    default:
+                    System.err.println(("One of the day in the list is not a day of the week!"));
+                }
+                localWoh.setOpeningHour(day, Integer.parseInt(split[1]), Integer.parseInt(split[2]));
+                localWoh.setOpeningHour(day, Integer.parseInt(split[1]), Integer.parseInt(split[2]));
+            }
+            for(Boolean b : isEdited){
+                if(!b){
+                    System.err.println("Warning: Not all day were loaded. Day not loaded will be set to [00:00,00:00]");
+                    break;
+                }
+            }
+            this.getCopy(localWoh);
+
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    public void savePlanningFile(String path){
+        //TODO:Save a file copy of a PlanningFile
+    }
+
+    private void getCopy(WeeklyOpeningHours woh){
+        for(int day = 1; day < 8; day ++){
+            this.setOpeningHour(day, woh.dailyOpeningHours.get(day).get(0),  woh.dailyOpeningMinutes.get(day).get(0));
+            this.setClosingHour(day, woh.dailyOpeningHours.get(day).get(1),  woh.dailyOpeningMinutes.get(day).get(1));
+        }
+    }
+
+    public WeeklyOpeningHours copy(){
+        WeeklyOpeningHours newWoh = new WeeklyOpeningHours();
+        for(int day = 1; day < 8; day ++){
+            newWoh.setOpeningHour(day, this.dailyOpeningHours.get(day).get(0),  this.dailyOpeningMinutes.get(day).get(0));
+            newWoh.setClosingHour(day, this.dailyOpeningHours.get(day).get(1),  this.dailyOpeningMinutes.get(day).get(1));
+        }
+        return newWoh;
     }
 }
